5a340cc8b1339a233229a4aa16c3102f8c8211fa
lordshtein <lordshtein@gmail.com>
2020-03-10 18:56:18 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-exports.min = function min (array) {
-  return 0;
+function compare(a, b) {
+    return a - b
 }
 
-exports.max = function max (array) {
-  return 0;
+function compareMax(a, b) {
+    return b - a
 }
 
-exports.avg = function avg (array) {
-  return 0;
+function reducer(summ, el) {
+    return summ + el
+}
+
+exports.min = function min(array) {
+    if (!Array.isArray(array) || !array.length) return 0
+    array.sort(compare)
+    return array[0];
+}
+
+exports.max = function max(array) {
+    if (!Array.isArray(array) || !array.length) return 0
+    array.sort(compareMax)
+    return array[0];
+}
+
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || !array.length) return 0
+    return array.reduce(reducer) / array.length
 }
